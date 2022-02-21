9f72f9c6d92b346531309b03dcd58b211fc9a131
sanky2020 <sankyk2020@gmail.com>
2022-02-21 11:55:07 +0530

    Updated Accordian component

--- src/components/Accordian.js
+++ src/components/Accordian.js
@@ -2,16 +2,34 @@ import { Accordion, AccordionDetails, AccordionSummary, Typography,  } from '@mu
 import React from 'react'
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore"
 
+
+
 function Accordian() {
   return (
-    <div>
-        <Accordion sx={{width:"50%", margin:"0 auto"}}>
+    <div className='center'>
+    <Typography style={{marginTop:50}} variant='h4'>Accordian</Typography>
+        <Accordion sx={{width:"50%", margin:"0 auto"}} style={{margin: "10px auto"}}>
         <AccordionSummary
           expandIcon={<ExpandMoreIcon />}
           
         >
-          <Typography >Accordion 1</Typography>
-          <Typography sx={{width:'50%', border:1, marginLeft:50}}>Summary 1</Typography>
+          <Typography style={{width:"100%"}}>Accordion 1</Typography>
+          <Typography sx={{width:'50%', marginLeft:50}}>Summary 1</Typography>
+        </AccordionSummary>
+        <AccordionDetails >
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion sx={{width:"50%", margin:"0 auto"}} style={{margin: "10px auto"}}>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          
+        >
+          <Typography style={{width:"100%"}}>Accordion 2</Typography>
+          <Typography sx={{width:'50%', marginLeft:50}}>Summary 2</Typography>
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
@@ -20,12 +38,13 @@ function Accordian() {
           </Typography>
         </AccordionDetails>
       </Accordion>
-      <Accordion>
+      <Accordion sx={{width:"50%", margin:"0 auto"}} disabled style={{margin: "10px auto"}}>
         <AccordionSummary
           expandIcon={<ExpandMoreIcon />}
           
         >
-          <Typography>Accordion 2</Typography>
+          <Typography style={{width:"100%"}}>Disabled Accordion</Typography>
+          <Typography sx={{width:'60%', marginLeft:50 }}>Summary 3</Typography>
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
@@ -34,13 +53,20 @@ function Accordian() {
           </Typography>
         </AccordionDetails>
       </Accordion>
-      <Accordion disabled>
+      <Accordion sx={{width:"50%", margin:"0 auto"}} style={{margin: "10px auto"}}>
         <AccordionSummary
           expandIcon={<ExpandMoreIcon />}
           
         >
-          <Typography>Disabled Accordion</Typography>
+          <Typography style={{width:"100%"}}>Accordion 4</Typography>
+          <Typography sx={{width:'50%', marginLeft:50}}>Summary 4</Typography>
         </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
       </Accordion>
     </div>
   )
