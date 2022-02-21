5752931df321ac7e0f80149f906408f491b3c092
sanky2020 <sankyk2020@gmail.com>
2022-02-21 12:19:55 +0530

    updated controlled accordian to the list

--- src/components/Accordian.js
+++ src/components/Accordian.js
@@ -1,10 +1,13 @@
 import { Accordion, AccordionDetails, AccordionSummary, Typography,  } from '@mui/material'
-import React from 'react'
+import React, {useState} from 'react'
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore"
 
 
 
 function Accordian() {
+
+  const [expandAccord, setExpandAccord] = useState(false)
+
   return (
     <div className='center'>
     <Typography style={{marginTop:50}} variant='h4'>Accordian</Typography>
@@ -60,6 +63,37 @@ function Accordian() {
         >
           <Typography style={{width:"100%"}}>Accordion 4</Typography>
           <Typography sx={{width:'50%', marginLeft:50}}>Summary 4</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Typography variant='h6'>Controlled Accordian</Typography>
+      <Accordion sx={{width:"50%", margin:"0 auto"}} style={{margin: "10px auto"}} expanded={expandAccord === "accord5"} onChange={()=> setExpandAccord("accord5") }>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          
+        >
+          <Typography style={{width:"100%"}}>Accordion 5</Typography>
+          <Typography sx={{width:'50%', marginLeft:50}}>Summary 5</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion sx={{width:"50%", margin:"0 auto"}} style={{margin: "10px auto"}} expanded={expandAccord === "accord6"} onChange={()=> setExpandAccord("accord6") }>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          
+        >
+          <Typography style={{width:"100%"}}>Accordion 6</Typography>
+          <Typography sx={{width:'50%', marginLeft:50}}>Summary 6</Typography>
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
